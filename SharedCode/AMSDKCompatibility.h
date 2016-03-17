75a2334ff4b7839ed52f16eeccdde04166d56d69
Sean McBride <sean@rogue-research.com>
2016-03-17 18:33:29 -0400

    Add #error if deployment < 10.6, matching xib

--- SharedCode/AMSDKCompatibility.h
+++ SharedCode/AMSDKCompatibility.h
@@ -2,13 +2,18 @@
 //  AMSDKCompatibility.h
 //
 //  Created by Nick Zitzmann on 2007-10-22.
-//  Copyright (c) 2007-2015 Andreas Mayer. All rights reserved.
+//  Copyright (c) 2007-2016 Andreas Mayer. All rights reserved.
 //
 
 // AMSerialPort uses some features from newer SDKs.
 // This allows older SDKs to be used by adding compatibility wrappers.
 
 #import <Foundation/Foundation.h>
+
+// We don't support deployment older than 10.6.
+#if MAC_OS_X_VERSION_MIN_REQUIRED < 1060
+	#error Minimum deployment supported is 10.6.
+#endif
 
 // We don't support SDKs older than 10.7.
 #if MAC_OS_X_VERSION_MAX_ALLOWED < 1070
